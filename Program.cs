0fed5c34d818588c6eee0e38c91fdbf2efed4c68
Raphael Valencio <raphael.martinez@estudante.ifms.edu.br>
2024-10-12 23:34:13 -0400

    feat: add MySQL connection string
    Configured the connection string for MySQL in appsettings.json.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var mySqlConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseMySql(mySqlConnectionString, ServerVersion.AutoDetect(mySqlConnectionString)));
 
 var app = builder.Build();
 
